7f7785470a0c312d822896aecdbdcf9e950e9dbe
Max <maxrrice@hotmail.com>
2023-07-15 14:07:52 -0500

    fetch functions and simple click listener

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,22 +1,38 @@
 var apiKey = "a061eb6370c867e1da420f61897c46e5"
+var searchBar = document.getElementById("search-bar")
+var searchButton = document.getElementById("search-button")
+
+var sunny = "☀"
 
 
-// how to fetch 5-day forecast from API
-fetch ("https://api.openweathermap.org/data/2.5/forecast?lat=42.7261&lon=87.7829&appid="+apiKey)
-.then(function(response) {
-    console.log(response)
-    return response.json()
-})
-.then(function(forecastData) {
-    console.log(forecastData)
-})
+// fetch 5-day forecast from API
+function getForecast(cityName) {
+    fetch ("https://api.openweathermap.org/data/2.5/forecast?q="+cityName+"&appid="+apiKey)
+        .then(function(response) {
+            console.log(response)
+            return response.json()
+        })
+        .then(function(forecastData) {
+            console.log(forecastData)
+        })
+}
 
-// TODO how to fetch current weather from API
-fetch ("https://api.openweathermap.org/data/2.5/weather?q=Milwaukee&appid="+apiKey)
-.then(function(response) {
-    console.log(response)
-    return response.json()
-})
-.then(function(forecastData) {
-    console.log(forecastData)
+// fetch current weather from API
+function getCurrentWeather(cityName) {
+    fetch ("https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+apiKey)
+        .then(function(response) {
+            console.log(response)
+            return response.json()
+        })
+        .then(function(forecastData) {
+            console.log(forecastData)
+        })
+}
+
+// grab text from search input
+searchButton.addEventListener("click", function() {
+    var inputValue = searchBar.value
+    // call for current weather using city name
+    getCurrentWeather(inputValue)
+    getForecast(inputValue)
 })
